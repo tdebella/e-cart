e4db84c9ca75a946b73a076d754532cb5e63d632
tesfaye robelle <tdebella91@gail.com>
2022-12-01 11:47:46 -0500

    add README.md

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,15 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 
+
 //i import the Provider cmp and wraps the App cmp & its child components in it.
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
   </React.StrictMode>
 );
